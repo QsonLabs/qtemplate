da7557b5b1baf8f3d2b808205cb1cbe6fe74588c
Chris O'Connor <45886288+cdoconno@users.noreply.github.com>
2020-12-27 16:32:58 -0600

    Fixed flake8 formatting

--- qtemplate/utils/config.py
+++ qtemplate/utils/config.py
@@ -21,11 +21,13 @@ def default_config():
         },
     }
 
+
 def merge(default, config):
     """Merge two configs and returns as dict"""
     new = default.copy()
     new.update(config)
     return new
+
 
 # --------------------------------------------------------------
 # Config Loading
@@ -34,25 +36,31 @@ def load(uri):
     """Load an external document from a store"""
     pass
 
+
 def resolve_loader(uri):
     """Resolve the appropriate loader function"""
-    pass 
+    pass
+
 
 def resolve_scheme(scheme):
     """Ensures valid scheme and required URI fields per scheme"""
-    pass 
+    pass
+
 
 def load_file_yaml():
     """Load a document from yaml"""
     pass
 
+
 def load_file_ini():
     """Load a document from ini config"""
     pass
 
+
 def load_file_json():
     """Load a document from a json file"""
     pass
+
 
 def load_https():
     """Load a document from an https respource"""
